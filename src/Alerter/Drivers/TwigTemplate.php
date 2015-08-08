be0e61c489f4acb5525f9a7f558361203b62b2be
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2015-08-08 23:00:30 +0500

    Added a namespace

--- src/Alerter/Drivers/TwigTemplate.php
+++ src/Alerter/Drivers/TwigTemplate.php
@@ -6,9 +6,9 @@ class TwigTemplate implements Template
 {
 	public function render($view, $data=[])
 	{
-		$loader = new Twig_Loader_Filesystem(__DIR__);
+		$loader = new \Twig_Loader_Filesystem(__DIR__);
 
-		$twig = new Twig_Environment($loader);
+		$twig = new \Twig_Environment($loader);
 
 		return $twig->render($view, $data);
 	}
